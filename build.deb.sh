4b3dc0e96a51b26536ba1038f50c50d5bc811fac
Kitsune Solar <kitsune.solar@gmail.com>
2021-09-14 11:57:48 +0300

    2021-09-14 08:57:48

--- build.deb.sh
+++ build.deb.sh
@@ -1,9 +1,29 @@
 #!/usr/bin/bash
 
-#PKG_NAME="${1}"
-#PKG_VER="${2}"
-#PKG_ARCH="${3}"
-#PKG_DEPENDS="${4}"
+structure() {
+  echo -n "Package Name [ENTER]: "; read -r PKG_NAME
+  echo -n "Package Version (ex.: 1.0.0) [ENTER]: "; read -r PKG_VER
+  echo -n "Package Arch (ex.: all/any/amd64/i386) [ENTER]: "; read -r PKG_ARCH
+  echo -n "Package Depends (ex.: pkg1, pkg2, pkg3) [ENTER]: "; read -r PKG_DEPENDS
+  echo -n "Package Section (ex.: pkg1, pkg2, pkg3) [ENTER]: "; read -r PKG_SECTION
+  echo -n "Package Priority (ex.: pkg1, pkg2, pkg3) [ENTER]: "; read -r PKG_PRIORITY
+
+  [[ -z "${PKG_NAME}" ]] && PKG_NAME="ext-example-pkg"
+  [[ -z "${PKG_VER}" ]] && PKG_VER="1.0.0"
+  [[ -z "${PKG_ARCH}" ]] && PKG_ARCH="all"
+  [[ -z "${PKG_SECTION}" ]] && PKG_SECTION="admin"
+  [[ -z "${PKG_PRIORITY}" ]] && PKG_PRIORITY="optional"
+
+  PKG_REV="1"
+
+  cat="$( command -v cat )"
+  chmod="$( command -v chmod )"
+  date="$( command -v date )"
+  mkdir="$( command -v mkdir )"
+  tar="$( command -v tar )"
+
+  _deb_dirs && _deb_files && _obs_files && _pack
+}
 
 _changelog_ts() {
   ${date} -u '+%a, %d %b %Y %T %z'
@@ -101,31 +121,6 @@ _pack() {
   pushd "$( pwd )/${PKG_NAME}" \
     && ${tar} -cJf "${PKG_NAME}_${PKG_VER}.orig.tar.xz" "${PKG_NAME}-${PKG_VER}" \
     && popd || exit 1
-}
-
-structure() {
-  echo -n "Package Name [ENTER]: "; read -r PKG_NAME
-  echo -n "Package Version (ex.: 1.0.0) [ENTER]: "; read -r PKG_VER
-  echo -n "Package Arch (ex.: all/any/amd64/i386) [ENTER]: "; read -r PKG_ARCH
-  echo -n "Package Depends (ex.: pkg1, pkg2, pkg3) [ENTER]: "; read -r PKG_DEPENDS
-  echo -n "Package Section (ex.: pkg1, pkg2, pkg3) [ENTER]: "; read -r PKG_SECTION
-  echo -n "Package Priority (ex.: pkg1, pkg2, pkg3) [ENTER]: "; read -r PKG_PRIORITY
-
-  [[ -z "${PKG_NAME}" ]] && PKG_NAME="ext-example-pkg"
-  [[ -z "${PKG_VER}" ]] && PKG_VER="1.0.0"
-  [[ -z "${PKG_ARCH}" ]] && PKG_ARCH="all"
-  [[ -z "${PKG_SECTION}" ]] && PKG_SECTION="admin"
-  [[ -z "${PKG_PRIORITY}" ]] && PKG_PRIORITY="optional"
-
-  PKG_REV="1"
-
-  cat="$( command -v cat )"
-  chmod="$( command -v chmod )"
-  date="$( command -v date )"
-  mkdir="$( command -v mkdir )"
-  tar="$( command -v tar )"
-
-  _deb_dirs && _deb_files && _obs_files && _pack
 }
 
 "$@"
